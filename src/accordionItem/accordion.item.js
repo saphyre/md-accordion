18fe1abce18e92dfb5dc07d283857202e8badaba
Anderson Leal <andersonofl@gmail.com>
2016-03-23 21:28:10 -0300

    - Svg-morpheus dependency - Removed h2 on title transclusion

--- src/accordionItem/accordion.item.js
+++ src/accordionItem/accordion.item.js
@@ -17,7 +17,7 @@
         template :
             '<div class="md-accordion" layout="column">\
                 <md-button class="md-primary accordion-toolbar" ng-click="$ctrl.toggle()" layout="row">\
-                    <h2 flex class="md-flex" ng-transclude="title"></h2>\
+                    <div flex ng-transclude="title" layout="row" layout-align="start start"></div>\
                     <ng-md-icon icon="{{ $ctrl.expanded ? \'expand_less\' : \'expand_more\' }}"></ng-md-icon>\
                 </md-button>\
                 <md-content flex layout-padding ng-transclude="content" ng-show="$ctrl.expanded"></md-content>\
